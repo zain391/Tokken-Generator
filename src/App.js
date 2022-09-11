a8a73120993ebb588d3b449449a8d239d6490c08
zain391 <zainqamar391@gmail.com>
2022-09-11 14:28:47 +0500

    removed the folders from src to reuse it for understanding the formik

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import './App.css';
 import InvoiceForm from './components/UserForms/invoiceForm';
-import { BrowserRouter,Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+// import { BrowserRouter,Routes, Route } from 'react-router-dom';
+// import Home from './pages/home';
+// import Header from './components/Header/Header';
+// import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
-      {/* <InvoiceForm/> */}
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Header/>
         <Routes>
           <Route path='/' element={<Home/>}> </Route>
           <Route path='/invoices' element={<InvoiceForm/>}> </Route>
         </Routes>
         <Footer/>
-      </BrowserRouter>
-    
+      </BrowserRouter> */}
+    <InvoiceForm/>
     </div>
   );
 }
