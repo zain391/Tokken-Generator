846dafe788d420c782e96c172366d5d5e06ea640
zain391 <zainqamar391@gmail.com>
2022-09-12 14:59:55 +0500

    Delete done have to start wok from the start

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import './App.css';
-import InvoiceForm from './components/UserForms/invoiceForm';
-import { BrowserRouter,Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header/>
-        <Routes>
-          <Route path='/' element={<Home/>}> </Route>
-          <Route path='/invoices' element={<InvoiceForm/>}> </Route>
-        </Routes>
-        <Footer/>
-      </BrowserRouter>
+     
     </div>
   );
 }
