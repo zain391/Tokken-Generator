9243c91c675ee6d37d07eb4ecc145f8acf58c086
zain391 <zainqamar391@gmail.com>
2022-09-13 16:00:35 +0500

    deleted the Formik Form now going to work on Token generator Form using Formik

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import './App.css';
-import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
-import ForkimForm from './components/Forms/FormikForm.jsx';
-import Form from './components/Forms/Forms';
-import Navbar from './components/Header/Navbar';
 function App() {
   return (
     <div className="App">
-      <Router>
-      <Navbar/>
-        <Routes>
-        
-          <Route path="/" element={<ForkimForm/>} />
-          <Route path="/form" element={<Form/>} />
-        </Routes>
-      </Router>
+      
     </div>
   );
 }
--- src/components/Forms/Inputclasses.js
+++
@@ -1,5 +0,0 @@
-export const inputClasse=[
-    {
-        className:"mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-    },
-]
