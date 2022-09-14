fbb20fa87addc14fd2b86052dd82515a6f7a2b29
zain391 <zainqamar391@gmail.com>
2022-09-14 21:43:15 +0500

    going to work on the Formik Field array to complete the Tokken generator app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import TokkenDeploy from './components/Tokentask2/TokkenDeploy';
 import TokkenTask2 from './components/Tokentask2/TokkenTask2';
 function App() {
   return (
     <div className="App">
       <TokkenTask2/>
-      <TokkenDeploy/>
     </div>
   );
 }
